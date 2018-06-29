2698ec58fc95338b2589de6ebfd87be4603f698d
Job Mos <jobmos95@gmail.com>
2018-06-29 09:52:46 +0000

    updated styling and more functionality

--- picker.py
+++ picker.py
@@ -1,7 +1,8 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, font
 from PIL import ImageTk, Image
 from tkcalendar import Calendar
+import print_text
 
 
 class CalenderUI():
@@ -9,7 +10,7 @@ class CalenderUI():
         self.top = tk.Toplevel(root)
 
         self.cal = Calendar(self.top, font="Arial 14", selectmode='day',
-                            cursor="hand1", year=2018, month=2, day=5)
+                            cursor="hand1", year=2016, month=6, day=11)
         self.cal.pack(fill="both", expand=True)
         ttk.Button(self.top, text="ok", command=self.print_sel).pack()
         ttk.Button(self.top, text="exit", command=self.quit1).pack()
@@ -26,79 +27,101 @@ class CalenderUI():
 
 
 class App:
-    def __init__(self):
-        self.root = tk.Tk()
-        s = ttk.Style(self.root)
+    def __init__(self, master):
+        self.master = master
+        master.title("PyTSI User Interface")
+
+        s = ttk.Style(self.master)
         s.theme_use('clam')
 
         self.img_folder = '/nobackup/users/mos/data/TSI/DBASE/201606/20160611_tsi-cabauw_realtime/'
 
+        # background colors
+        background_color = '#f4f4f4'
+        self.master.configure(bg=background_color)
+
+        # button colors
+        button_color = '#bababa'
+
+        # fonts
+        default_font = font.nametofont('TkDefaultFont')
+        default_font.configure(size=12)
+        self.master.option_add("*Font", default_font)
+
+        # sizes
         size_x = 800
         size_y = 500
-        self.root.geometry('{}x{}'.format(size_x, size_y))
+        self.master.geometry('{}x{}'.format(size_x, size_y))
 
         self.date = '2016-06-11'
-        self.time = ''
+        self.time = '12:00'
 
         # image 1
         img = ImageTk.PhotoImage(Image.open('images/20170828112200.jpg'))
-        self.panel1 = tk.Label(self.root, image=img)
+        self.panel1 = tk.Label(self.master, image=img)
         self.panel1.grid(row=3, column=1, rowspan=2, sticky='NSEW')
 
         # image 2
         img2 = ImageTk.PhotoImage(Image.open('images/20170828112200.png'))
-        self.panel2 = tk.Label(self.root, image=img2)
+        self.panel2 = tk.Label(self.master, image=img2)
         self.panel2.grid(row=3, column=2, rowspan=2, sticky='NSEW')
 
         # calendar
-        cal_button = ttk.Button(self.root, text='Select date', command=self.get_date)
+        cal_button = tk.Button(self.master, text='Select date', command=self.get_date, activebackground=button_color,
+                               activeforeground='white')
         cal_button.grid(row=3, column=4, columnspan=2, sticky='EW')
 
         # time
-        self.e = tk.Entry(self.root, width=10)
-        self.e.focus_set()
+        self.e = tk.Entry(self.master, width=10)
+        # self.e.focus_set()
         self.e.grid(row=4, column=4)
         self.e.delete(0, tk.END)
-        self.e.insert(0, '12:00')
-        self.e.bind('<Control-KeyRelease-a>')
+        self.e.insert(0, self.time)
+        self.e.bind('<Control-KeyRelease-a>', lambda evt: self.select_all(evt))
 
         # time button
-        time_button = tk.Button(self.root, text='Select time', width=10, command=self.get_time)
+        time_button = tk.Button(self.master, text='Select time', width=10, command=self.get_time,
+                                activebackground=button_color, activeforeground='white')
         time_button.grid(row=4, column=5)
 
         # title
-        title = tk.Text(self.root, height=1)
+        title = tk.Text(self.master, height=1)
         title.insert(tk.INSERT, 'Select a date and time')
         title.grid(row=1, column=1, columnspan=5)
 
         # info
-        info = tk.Text(self.root, height=3)
+        info = tk.Text(self.master, height=3, width=30)
         info.insert(tk.INSERT, 'Cloud cover:x \nAzimuth:x \nAltitude:x')
-        info.grid(row=6, column=1, columnspan=4)
+        info.grid(row=6, column=1, columnspan=1)
 
         # alignments
-        self.root.grid_rowconfigure(0, weight=1)
-        self.root.grid_rowconfigure(2, weight=1)
-        self.root.grid_rowconfigure(5, weight=1)
-        self.root.grid_rowconfigure(7, weight=1)
-        self.root.grid_columnconfigure(0, weight=1)
-        self.root.grid_columnconfigure(3, weight=1)
-        self.root.grid_columnconfigure(6, weight=1)
+        self.master.grid_rowconfigure(0, weight=1)
+        self.master.grid_rowconfigure(2, weight=1)
+        self.master.grid_rowconfigure(5, weight=1)
+        self.master.grid_rowconfigure(7, weight=1)
+        self.master.grid_columnconfigure(0, weight=1)
+        self.master.grid_columnconfigure(3, weight=1)
+        self.master.grid_columnconfigure(6, weight=1)
 
-        self.root.mainloop()
+        self.master.bind('<Return>', lambda evt: self.get_time(evt))
 
-    def get_date(self):
-        cal = CalenderUI(self.root)
-        self.root.wait_window(cal.top)
-        self.date = cal.date
+        root.mainloop()
+
+    def get_date(self, event=None):
+        cal = CalenderUI(self.master)
+        self.master.wait_window(cal.top)
+        self.date = str(cal.date)
         self.update_image()
 
-    def get_time(self):
+    def get_time(self, event=None):
         self.time = self.e.get()
-        self.update_image()
+        if self.time[2] != ':' or len(self.time) != 5:
+            print('Error: time format needs to follow specific format -> something like \'13:37\'')
+        else:
+            self.update_image()
 
-    def update_image(self):
-        date_time=self.date + self.time
+    def update_image(self, event=None):
+        date_time = self.date + self.time
         date_time = date_time.replace(':', '')
         date_time = date_time.replace('-', '')
         filename1 = date_time + '00.jpg'
@@ -111,4 +134,13 @@ class App:
         self.panel2.configure(image=img_new2)
         self.panel2.image = img_new2
 
-app = App()
+    @staticmethod
+    def select_all(event=None):
+        # select text
+        event.widget.select_range(0, 'end')
+        # move cursor to the end
+        event.widget.icursor('end')
+
+
+root = tk.Tk()
+app = App(root)
